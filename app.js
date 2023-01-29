65596d0b6cdf83944db37754bccc6696e8fd1265
Simon <simon@democracychain.org>
2023-01-29 22:33:28 +0000

    Add Google Auth

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-navigator.serviceWorker.register("/worker.js")
+navigator.serviceWorker.register("/worker.js");
 
 // This variable will save the event for later use.
 let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  console.log("beforeinstallprompt")
+window.addEventListener("beforeinstallprompt", (e) => {
+  console.log("beforeinstallprompt");
   // Prevents the default mini-infobar or install dialog from appearing on mobile
-  
+
   // e.preventDefault();
-  
+
   // Save the event because you'll need to trigger it later.
-  
+
   // deferredPrompt = e;
-  
+
   // Show your customized install prompt for your PWA
   // Your own UI doesn't have to be a single element, you
   // can have buttons in different locations, or wait to prompt
@@ -20,18 +20,111 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // showInAppInstallPromotion();
 });
 
-var boxes = document.getElementsByClassName("button")
-for(var i = 0; i < boxes.length; i++) {
-  console.log(boxes[i])
-  boxes[i].onclick = function (id, evt) { 
-    console.log(evt.target.parent)
-    if(evt.target.id === "votar") {
-      document.location = "/votar.html"
+var boxes = document.getElementsByClassName("button");
+for (var i = 0; i < boxes.length; i++) {
+  console.log(boxes[i]);
+  boxes[i].onclick = function (id, evt) {
+    console.log(evt.target.parent);
+    if (evt.target.id === "votar") {
+      document.location = "/votar.html";
     }
-    if(evt.target.id === "back") {
-      history.back()
+    if (evt.target.id === "back") {
+      history.back();
     }
-   }.bind(null, boxes[i].id)
+  }.bind(null, boxes[i].id);
 }
 
-console.log("INITED")
+function hideAll(className) {
+  var boxes = document.getElementsByClassName(className);
+  console.log("hiding", boxes)
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].classList.add("d-none")
+  }
+}
+
+function showAll(className) {
+  var boxes = document.getElementsByClassName(className);
+  console.log("showing", boxes)
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].classList.remove("d-none")
+  }
+}
+
+console.log("INITED");
+
+function setValuesToUI(user) {
+  document.getElementById("userPicture").src = user.picture;
+  document.getElementById("given_name").innerText = user.given_name;
+  // document.getElementById("family_name").innerText = user.family_name;
+  document.getElementById("googleLoginButton").style = "display: none;";
+  // document.getElementById("userPicture").style = "display: block;";
+
+  hideAll("loginUI")
+  showAll("pagMain")
+
+}
+
+async function handleCredentialResponse(response) {
+  console.log("Encoded JWT ID token: ", response.credential);
+  let googleUser = await fetch(
+    `https://auth.cloud.democraid.com/login/google?token=${response.credential}`,
+    {
+      credentials: "include",
+    }
+  );
+  user = await googleUser.json();
+
+  if (user.email) {
+    setValuesToUI(user);
+  }
+}
+
+window.onload = async function () {
+  console.log("LOAD!");
+  let userResponse = await fetch(`https://auth.cloud.democraid.com/user.json`, {
+    credentials: "include",
+  });
+  user = await userResponse.json();
+
+  console.log("USER", user);
+
+  // main();
+
+  if (!user.email) {
+    google.accounts.id.initialize({
+      client_id:
+        "371666599049-0fopffs7jmh4i791o9u6brtqlattvci5.apps.googleusercontent.com",
+      callback: handleCredentialResponse,
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("googleLoginButton"),
+      { theme: "outline" }
+    );
+
+    // setTimeout(() => {
+    //   console.log("prompt!");
+    //   google.accounts.id.prompt((notification) => {
+    //     console.log(
+    //       "notification.isNotDisplayed()",
+    //       notification.isNotDisplayed()
+    //     );
+    //     console.log(
+    //       "notification.isSkippedMoment()",
+    //       notification.isSkippedMoment()
+    //     );
+    //     if (
+    //       notification.isNotDisplayed() ||
+    //       notification.isSkippedMoment()
+    //     ) {
+    //       google.accounts.id.renderButton(
+    //         document.getElementById("googleLoginButton"),
+    //         { theme: "outline" }
+    //       );
+    //     }
+    //   }); // also display the One Tap dialog
+    // }, 1000);
+  } else {
+    setValuesToUI(user);
+  }
+};
---
+++ replayToken.js
@@ -0,0 +1,17 @@
+fetch("https://auth.cloud.democraid.com/login/google?token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjI3NDA1MmEyYjY0NDg3NDU3NjRlNzJjMzU5MDk3MWQ5MGNmYjU4NWEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJuYmYiOjE2NzUwMjc3MTksImF1ZCI6IjM3MTY2NjU5OTA0OS0wZm9wZmZzN2ptaDRpNzkxbzl1NmJydHFsYXR0dmNpNS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwNTQxMDU3ODUwODk1MjE4MzMwMCIsImVtYWlsIjoiZ3VlcnJlcm9jYXJsb3NAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF6cCI6IjM3MTY2NjU5OTA0OS0wZm9wZmZzN2ptaDRpNzkxbzl1NmJydHFsYXR0dmNpNS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsIm5hbWUiOiJDYXJsb3MgR3VlcnJlcm8iLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUVkRlRwNHIxbThMd1R4YnRlZjBlM2R6eVAwODBsZklYNEJ6UmRvb3M4OGtyMzg9czk2LWMiLCJnaXZlbl9uYW1lIjoiQ2FybG9zIiwiZmFtaWx5X25hbWUiOiJHdWVycmVybyIsImlhdCI6MTY3NTAyODAxOSwiZXhwIjoxNjc1MDMxNjE5LCJqdGkiOiI4MzcwZjRiNjc3MzdiNTEwZjc4MThmMGJlNDMxOGEyMmQ2MDcyOTc4In0.vkV2IE1l-xxEzhw_f6JFzqFcOBmG0yxsWeytV2rt9b5kpCf24eAELZQgeNCGgHlVHYCblytqqIw_HnDnfOIZ6UksIH5b4gn0D1TJQyefnba6Cx1pPD0jE6GhWpHiT2up0aqubXn0J0PPWp9BFm_yHPJS2DCvTgmfH552XJwHx2_-d6Lvt5P10O8VKOgxtkyGnMOH_z84Ne0I_mwAOmAMAA6HGI1i9GIu-4lLCtxxHx1_O4oKTP9AVHbPGrhs321nx-yLbbvLBM-E_aDfz_7172KBpBPH4QYdz5AjcerEBZsNNBkGUVRPCqv98TE6Buj93QYVeGkKK8xRxmRxHEu_4w", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "es,en-US;q=0.9,en;q=0.8",
+    "sec-ch-ua": "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site",
+    "sec-gpc": "1",
+    "Referer": "https://c9374b80179271.lhr.life/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": null,
+  "method": "GET"
+});
