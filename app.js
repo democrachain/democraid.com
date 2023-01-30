a37d3d98edf7f4326b3ee0c9daf0ebfe29c1107e
Simon <simon@democracychain.org>
2023-01-30 03:50:23 +0000

    Prod detector

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 navigator.serviceWorker.register("/worker.js");
-var savedUser = localStorage.getItem('user')
-var user = savedUser ? JSON.parse(savedUser) : {}
+
+var user = {
+  iss: "https://accounts.google.com",
+  nbf: 1675036818,
+  aud: "371666599049-0fopffs7jmh4i791o9u6brtqlattvci5.apps.googleusercontent.com",
+  sub: "105410578508952183300",
+  email: "guerrerocarlos@gmail.com",
+  email_verified: true,
+  azp: "371666599049-0fopffs7jmh4i791o9u6brtqlattvci5.apps.googleusercontent.com",
+  name: "Carlos Guerrero",
+  picture:
+    "https://lh3.googleusercontent.com/a/AEdFTp4r1m8LwTxbtef0e3dzyP080lfIX4BzRdoos88kr38=s96-c",
+  given_name: "Carlos",
+  family_name: "Guerrero",
+  iat: 1675037118,
+  exp: 1675040718,
+  jti: "921280f2cf730d34f8bf6dc332b07b7e69ebd9f7",
+  democraid: "ueec5d4c973",
+  keys: {
+    priv: "-----BEGIN RSA PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDWR1SahFRee/pvhBvxrX/beIFpfOJl4MMMPKawawX0CKEOj48r7HYfx6mS/gU6Hbu/ueY6Dc4oCvk0Oi6brDIj31KQ+CQLqmJLkVVm+ASwvyyN4Ag38qlvfns2vTHUM7+eokj3cA+cW/19mhg+iUFRP0v4g1LkpaAZ7q9izf7TrxEDPMJuo2NGnJIMKw9bHdR41tf3Fk8Kbc66VWZm5IdPRrgATRkulyWo9xhhhQMkf5jPkBciQRbshRs9wwFHq8gKDmOpwCnX0HzB4xV2tOPBD2XlunGsVpyJq5bheId1YQ1ZFCQBV2/ZNZNNdnK92OUvx2ffAhALFdxvuS+/S1rHAgMBAAECggEBAMzPVyVt0HqTA9dtLYx96lSrRP6/+GGqSKQ8oJLFaOZNDSwuuYkhNfLAU+wE0pgu7VKfbFgW8/LAMlviNMRk/XPeNDwOgd2ImPVjz35hF5Kc2Agvl4tqbNr9yOWURrluUPxeX2HqgXFRV+Si0gOMC5uN5Z5+X0/eoz3GL2tAPA61aUjhDkRF0ehwIl70XLD+C4U8nCfBZNtiP8eM06jMNFtBOzsU4vDaR9Wa9jyev5qaQuT1Vau4ncYUXcQZPWZrLdNw7Gxj1j6o2ijh1XoFrz8Z3Z3A/K1wneLikHQUmlOk90ard/FQu6nD8wGKqDgw5KmRLAPmabyfXKydqqcuduECgYEA9ejrDqDAnCyOlfQd7B/RH3cYR5O7IFUK+Cf1xnzA748rfmyHxXZaYeM98ppIJJ/yg9hfmQiFneZqIslSXA8pomUEE4i0+lhmicrFJG6r6o4i61sFak75mueQeAF2awnNwOv87F4zRBcJHsxZYdGTKiHASWTdaswV3v0nS/DMRVECgYEA3xInAdX8SWQwS0ZjaTdfkc1Y5UTf7ksqiK/N1gqc9yZ8aBRVDxqMTIEVHwdTo8wn+AWo5ySOmQ6pmYuvg66Str9T1bEjbxPrPDBxN2Thx7V5uRmHi5QWks9ZTb1V9TPUnBVDQxwYT6m0HuvL1YrqqVYzdlrYRv4W15NP0E8o+JcCgYEAlxk4HXPfBddHAZLPNABU0d2u2IRIrdQzekmrdfu/3TL+iZ8MSeOwI4eqz7/G5mI1dJfmHbUjzOMAgkFrzs8uSO+C8rHEajMZRj+GpR0vm18cy7rQ+AJw0qLInURgy+JpP8qBTYeQPp1c6ESuAzHwGCpG6ZIFjxK5uZuZosrh6RECgYABPE2BAhlqoqZt1E76pzbdTODgLDh9TmdG6IpgVCC4cbsgrHQoKEJ8rf5a9KRu9NOH2SgtV5N+n4kq844eUZo4bujc9yU2GUslQzNtVh62B3hMISsSB9j0KTfaaWEPgaD82FVOCtrLnioEPdQcM6/HDlYsoqYuBnxD52n6wmV8swKBgQD0wM+yaLDQotApoXdHUFbu3oFqdHHF/cA4vs08tJvCH0SxYWd+3n9jj7QrUMEANq2s+1YRsZjKcN1U3qCa372IVnXMzyIrc+yNR62sbRWtL6JcXXzUy+yUf97fNIu+rbRmHjTD1B+2SMPMRNPXMFlKumnV8LqkliwUZXj9IwgZ+g==\n-----END RSA PRIVATE KEY-----",
+    pub: "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1kdUmoRUXnv6b4Qb8a1/23iBaXziZeDDDDymsGsF9AihDo+PK+x2H8epkv4FOh27v7nmOg3OKAr5NDoum6wyI99SkPgkC6piS5FVZvgEsL8sjeAIN/Kpb357Nr0x1DO/nqJI93APnFv9fZoYPolBUT9L+INS5KWgGe6vYs3+068RAzzCbqNjRpySDCsPWx3UeNbX9xZPCm3OulVmZuSHT0a4AE0ZLpclqPcYYYUDJH+Yz5AXIkEW7IUbPcMBR6vICg5jqcAp19B8weMVdrTjwQ9l5bpxrFaciauW4XiHdWENWRQkAVdv2TWTTXZyvdjlL8dn3wIQCxXcb7kvv0taxwIDAQAB\n-----END PUBLIC KEY-----",
+  },
+};
+
+if(window.location.origin === 'https://democraid.com') {
+  var savedUser = localStorage.getItem("user")
+  user = savedUser ? JSON.parse(savedUser) : {};
+} 
 
 // This variable will save the event for later use.
 let deferredPrompt;
@@ -38,17 +64,17 @@ for (var i = 0; i < boxes.length; i++) {
 
 function hideAll(className) {
   var boxes = document.getElementsByClassName(className);
-  console.log("hiding", boxes)
+  console.log("hiding", boxes);
   for (var i = 0; i < boxes.length; i++) {
-    boxes[i].classList.add("d-none")
+    boxes[i].classList.add("d-none");
   }
 }
 
 function showAll(className) {
   var boxes = document.getElementsByClassName(className);
-  console.log("showing", boxes)
+  console.log("showing", boxes);
   for (var i = 0; i < boxes.length; i++) {
-    boxes[i].classList.remove("d-none")
+    boxes[i].classList.remove("d-none");
   }
 }
 
@@ -57,13 +83,13 @@ console.log("INITED");
 function setValuesToUI(user) {
   document.getElementById("userPicture").src = user.picture;
   document.getElementById("given_name").innerText = user.given_name;
+  document.getElementById("user_id").innerText = user.democraid;
   // document.getElementById("family_name").innerText = user.family_name;
   document.getElementById("googleLoginButton").style = "display: none;";
   // document.getElementById("userPicture").style = "display: block;";
 
-  hideAll("loginUI")
-  showAll("pagMain")
-
+  hideAll("loginUI");
+  showAll("pagMain");
 }
 
 async function handleCredentialResponse(response) {
@@ -76,10 +102,10 @@ async function handleCredentialResponse(response) {
   );
   user = await googleUser.json();
 
-  console.log("user", user)
+  console.log("user", user);
 
   if (user.email) {
-    localStorage.setItem("user", JSON.stringify(user))
+    localStorage.setItem("user", JSON.stringify(user));
     setValuesToUI(user);
   }
 }
@@ -96,6 +122,7 @@ window.onload = async function () {
   // main();
 
   if (!user.email) {
+    showAll("loginUI");
     google.accounts.id.initialize({
       client_id:
         "371666599049-0fopffs7jmh4i791o9u6brtqlattvci5.apps.googleusercontent.com",
